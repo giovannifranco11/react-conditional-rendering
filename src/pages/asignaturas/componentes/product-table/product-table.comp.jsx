6baac1118172386d0090910d18b1ca91be7ebbd7
giovannifranco <cgfranco72813@umanizales.edu.co>
2021-04-24 21:24:43 -0500

    Se complentan los tres cursos con 21 estudiantes. Se optimizan estilos css

--- src/pages/asignaturas/componentes/product-table/product-table.comp.jsx
+++ src/pages/asignaturas/componentes/product-table/product-table.comp.jsx
@@ -14,33 +14,59 @@ import hombre from './imagenes/hombre.png';
 const ProductTable = () => {
     return (
         <div className="product-table-container">
-           <ProductCategoryRow categoryName="Programación I"/>
+           <ProductCategoryRow categoryName="Programación IV"/>
            <div className="titulos">
                Nombre  Nota  Género  Foto
             </div>
            
                 <ProductRow product={{
                     name:"Giovanni Franco",
-                    nota:4.5,
+                    nota:1.5,
                     genero:"Masculino",
                     img:<img src={hombre} />,        
                 }
-                }/>
-                
-                
+                }/>               
 
                 <ProductRow product={{
-                    name:"Nataly Martínez",
-                    nota:5.0,
+                    name:"Natalia Martínez",
+                    nota:4.9,
                     genero:"Femenino",
                     img:<img src={mujer} />,
                 }}/>
 
                 <ProductRow product={{
-                    name:"Pepito Perez Gil",
+                    name:"Felipe Vargas Gil",
                     nota:2.6,
                     genero:"Masculino",
                     img:<img src={hombre} />,
+                }}/>
+                <ProductRow product={{
+                    name:"Sebastian Ruiz T.",
+                    nota:1.5,
+                    genero:"Masculino",
+                    img:<img src={hombre} />,        
+                }
+                }/>               
+
+                <ProductRow product={{
+                    name:"Sandra Rodríguez",
+                    nota:4.9,
+                    genero:"Femenino",
+                    img:<img src={mujer} />,
+                }}/>
+
+                <ProductRow product={{
+                    name:"Jeronimo Villada.",
+                    nota:2.6,
+                    genero:"Masculino",
+                    img:<img src={hombre} />,
+                }}/>
+
+                <ProductRow product={{
+                    name:"Esperanza Bernal",
+                    nota:4.9,
+                    genero:"Femenino",
+                    img:<img src={mujer} />,
                 }}/>
                               
 
@@ -48,54 +74,108 @@ const ProductTable = () => {
            <div className="titulos">
                Nombre  Nota  Género  Foto
             </div>
-                <ProductRow product={{
-                    name:"Giovanni Franco",
-                    nota:4.8,
+            <ProductRow product={{
+                    name:"Juan Frenandez",
+                    nota:2.8,
                     genero:"Masculino",
-                    img:<img src={hombre} />,              
+                    img:<img src={hombre} />,        
                 }
-                }/>
+                }/>               
 
                 <ProductRow product={{
-                    name:"Nataly Martínez",
-                    nota:3.5,
+                    name:"Camila Benedet",
+                    nota:4.9,
                     genero:"Femenino",
                     img:<img src={mujer} />,
                 }}/>
 
                 <ProductRow product={{
-                    name:"Pepito Perez Gil",
-                    nota:2.7,
+                    name:"Fernand Ochoa",
+                    nota:2.6,
+                    genero:"Masculino",
+                    img:<img src={hombre} />,
+                }}/>
+                <ProductRow product={{
+                    name:"Andres Figuera",
+                    nota:1.5,
+                    genero:"Masculino",
+                    img:<img src={hombre} />,        
+                }
+                }/>               
+
+                <ProductRow product={{
+                    name:"Mariana Vargas",
+                    nota:4.9,
+                    genero:"Femenino",
+                    img:<img src={mujer} />,
+                }}/>
+
+                <ProductRow product={{
+                    name:"Johan Cardona",
+                    nota:2.6,
                     genero:"Masculino",
                     img:<img src={hombre} />,
                 }}/>
 
-           <ProductCategoryRow categoryName="Algortimos"/>  
+                <ProductRow product={{
+                    name:"Manuela Charis",
+                    nota:4.9,
+                    genero:"Femenino",
+                    img:<img src={mujer} />,
+                }}/>
+
+           <ProductCategoryRow categoryName="Algoritmos"/>  
            <div className="titulos">
                Nombre  Nota  Género  Foto
             </div>
-                <ProductRow product={{
-                    name:"Giovanni Franco",
-                    nota:2.9,
+            <ProductRow product={{
+                    name:"Carlos Quintero",
+                    nota:4.5,
                     genero:"Masculino",
-                    img:<img src={hombre} />,
-                                
+                    img:<img src={hombre} />,        
                 }
-                }/>
-                
+                }/>               
 
                 <ProductRow product={{
-                    name:"Nataly Martínez",
-                    nota:3.9,
+                    name:"Vannesa Rivera.",
+                    nota:4.9,
                     genero:"Femenino",
                     img:<img src={mujer} />,
                 }}/>
 
                 <ProductRow product={{
-                    name:"Pepito Perez Gil",
-                    nota:2.5,
+                    name:"Bernardo Perez",
+                    nota:2.6,
                     genero:"Masculino",
                     img:<img src={hombre} />,
+                }}/>
+                <ProductRow product={{
+                    name:"Santiago Marti",
+                    nota:1.5,
+                    genero:"Masculino",
+                    img:<img src={hombre} />,        
+                }
+                }/>               
+
+                <ProductRow product={{
+                    name:"Mila Rodríguez",
+                    nota:4.9,
+                    genero:"Femenino",
+                    img:<img src={mujer} />,
+                }}/>
+
+                <ProductRow product={{
+                    name:"Jose Jairo Zaya",
+                    nota:2.6,
+                    genero:"Masculino",
+                    img:<img src={hombre} />,
+                }}/>
+
+                <ProductRow product={{
+                    name:"Stefania Bernal",
+                    nota:1.9,
+                    genero:"Femenino",
+                    img:<img src={mujer} />,
                 }}/>
                 
         </div>
